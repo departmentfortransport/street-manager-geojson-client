abde443e5e6e27796b0a34d98251f05a7148b044
Anne McAuley <a.mcauley@kainos.com>
2018-08-23 12:02:51 +0100

    add swa code to response for filtering in frontend and update promoter name to promoter organisation for consistency

--- dist/interfaces/workResponse.d.ts
+++ dist/interfaces/workResponse.d.ts
@@ -1,7 +1,8 @@
 import { WorkCategory, TrafficManagementType } from './referenceTypes';
 export interface WorkResponse {
     permit_id: number;
-    promoter_name: string;
+    promoter_organisation: string;
+    promoter_swa_code: string;
     work_category: WorkCategory;
     start_date: Date;
     end_date: Date;
